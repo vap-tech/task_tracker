1c7eb1aff32f8f507667bf1ed9e201a1447b5a8c
kot <bafomet2016vitt@gmail.com>
2023-12-03 23:59:56 +0300

    fix area

--- docker/app.sh
+++ docker/app.sh
@@ -2,6 +2,6 @@
 
 alembic upgrade head
 
-cd src
+cd src || { echo "Отсутствует каталог src"; exit 1; }
 
 gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
--- docker/celery.sh
+++ docker/celery.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd src
+cd src || { echo "Отсутствует каталог src"; exit 1; }
 
 if [[ "${1}" == "celery" ]]; then
-  celery --app=tasks.tasks:celery worker -l INFO
+  celery --app=src.report.report:celery worker
 elif [[ "${1}" == "flower" ]]; then
-  celery --app=tasks.tasks:celery flower
+  celery --app=src.report.report:celery flower
  fi
